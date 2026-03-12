f7fae52900875095cf4a2cebe756f81fac7c7b05
robbytrujillo <robbyikusuma88@gmail.com>
2026-03-12 07:23:07 +0700

    apss uks

--- application/views/admin/content/form.php
+++ application/views/admin/content/form.php
@@ -1,35 +1,28 @@
 <div class="main-content">
-<div class="section">
+    <div class="section">
 
-<div class="section-header">
-<h4>Form Import</h4>
-</div>
+        <div class="section-header">
+            <h4>Form Import</h4>
+        </div>
 
-<div class="section-body">
+        <div class="section-body">
+            <a href="<?= base_url('csv/format.csv'); ?>" class="btn btn-primary" style="border-radius: 30px;">
+                Download Format
+            </a>
+            <br>
+            <br>
 
-<a href="<?= base_url('csv/format.csv'); ?>" class="btn btn-primary">
-Download Format
-</a>
+            <form method="post" action="<?= base_url('Siswa/import') ?>" enctype="multipart/form-data">
 
-<br>
-<br>
+                <input type="file" name="file" class="form-control" required>
+                <br>
+                <button type="submit" class="btn btn-success" style="border-radius: 30px;">
+                    Import CSV
+                </button>
+            </form>
+            <br>
 
-<form method="post" action="<?= base_url('Siswa/import') ?>" enctype="multipart/form-data">
-
-<input type="file" name="file" class="form-control" required>
-
-<br>
-
-<button type="submit" class="btn btn-success">
-Import CSV
-</button>
-
-</form>
-
-
-<br>
-
-<!-- <form method="post" action="<?= base_url('Siswa/import_excel') ?>" enctype="multipart/form-data">
+            <!-- <form method="post" action="<?= base_url('Siswa/import_excel') ?>" enctype="multipart/form-data">
 
 <div class="form-group">
 <label>Upload Excel</label>
@@ -42,110 +35,108 @@ Import Excel
 
 </form> -->
 
-<br>
+            <br>
 
-<?php
-if(isset($_POST['preview'])){
+            <?php
+                if(isset($_POST['preview'])){
 
-if(isset($upload_error)){
-echo "<div class='alert alert-danger'>".$upload_error."</div>";
-die;
-}
+                if(isset($upload_error)){
+                    echo "<div class='alert alert-danger'>".$upload_error."</div>";
+                    die;
+                }
 
-echo "<form method='post' action='".site_url("Siswa/import")."'>";
+                echo "<form method='post' action='".site_url("Siswa/import")."'>";
 
-echo "<table class='table table-bordered table-striped'>";
+                echo "<table class='table table-bordered table-striped'>";
 
-echo "<thead>
-<tr>
-<th colspan='6'>Preview Data</th>
-</tr>
-<tr>
-<th>NISN</th>
-<th>Nama</th>
-<th>Jenis Kelamin</th>
-<th>Alamat</th>
-<th>Kelas</th>
-<th>Angkatan</th>
-</tr>
-</thead>";
+                echo "<thead>
+                        <tr>
+                            <th colspan='6'>Preview Data</th>
+                        </tr>
+                        <tr>
+                            <th>NISN</th>
+                            <th>Nama</th>
+                            <th>Jenis Kelamin</th>
+                            <th>Alamat</th>
+                            <th>Kelas</th>
+                            <th>Angkatan</th>
+                        </tr>
+                    </thead>";
 
-echo "<tbody>";
+                echo "<tbody>";
+                    $numrow = 1;
+                    $kosong = 0;
 
-$numrow = 1;
-$kosong = 0;
+                    foreach($sheet as $row){
 
-foreach($sheet as $row){
+                    $cellIterator = $row->getCellIterator();
+                    $cellIterator->setIterateOnlyExistingCells(false);
 
-$cellIterator = $row->getCellIterator();
-$cellIterator->setIterateOnlyExistingCells(false);
+                    $get = array();
 
-$get = array();
+                    foreach ($cellIterator as $cell) {
+                        array_push($get, $cell->getValue());
+                    }
 
-foreach ($cellIterator as $cell) {
-array_push($get, $cell->getValue());
-}
+                    $nis = $get[0];
+                    $nama = $get[1];
+                    $jk = $get[2];
+                    $alamat = $get[3];
+                    $kelas = $get[4];
+                    $angkatan = $get[5];
 
-$nis = $get[0];
-$nama = $get[1];
-$jk = $get[2];
-$alamat = $get[3];
-$kelas = $get[4];
-$angkatan = $get[5];
+                    if(empty($nis) && empty($nama) && empty($jk) && empty($alamat) && empty($kelas) && empty($angkatan))
+                    continue;
 
-if(empty($nis) && empty($nama) && empty($jk) && empty($alamat) && empty($kelas) && empty($angkatan))
-continue;
+                    if($numrow > 1){
 
-if($numrow > 1){
+                    $nis_td = (!empty($nis))? "" : "style='background:#E07171'";
+                    $nama_td = (!empty($nama))? "" : "style='background:#E07171'";
+                    $jk_td = (!empty($jk))? "" : "style='background:#E07171'";
+                    $alamat_td = (!empty($alamat))? "" : "style='background:#E07171'";
+                    $kelas_td = (!empty($kelas))? "" : "style='background:#E07171'";
+                    $angkatan_td = (!empty($angkatan))? "" : "style='background:#E07171'";
 
-$nis_td = (!empty($nis))? "" : "style='background:#E07171'";
-$nama_td = (!empty($nama))? "" : "style='background:#E07171'";
-$jk_td = (!empty($jk))? "" : "style='background:#E07171'";
-$alamat_td = (!empty($alamat))? "" : "style='background:#E07171'";
-$kelas_td = (!empty($kelas))? "" : "style='background:#E07171'";
-$angkatan_td = (!empty($angkatan))? "" : "style='background:#E07171'";
+                    if(empty($nis) or empty($nama) or empty($jk) or empty($alamat) or empty($kelas) or empty($angkatan)){
+                        $kosong++;
+                    }
 
-if(empty($nis) or empty($nama) or empty($jk) or empty($alamat) or empty($kelas) or empty($angkatan)){
-$kosong++;
-}
+                    echo "<tr>";
+                    echo "<td $nis_td>$nis</td>";
+                    echo "<td $nama_td>$nama</td>";
+                    echo "<td $jk_td>$jk</td>";
+                    echo "<td $alamat_td>$alamat</td>";
+                    echo "<td $kelas_td>$kelas</td>";
+                    echo "<td $angkatan_td>$angkatan</td>";
+                    echo "</tr>";
+                }
 
-echo "<tr>";
-echo "<td $nis_td>$nis</td>";
-echo "<td $nama_td>$nama</td>";
-echo "<td $jk_td>$jk</td>";
-echo "<td $alamat_td>$alamat</td>";
-echo "<td $kelas_td>$kelas</td>";
-echo "<td $angkatan_td>$angkatan</td>";
-echo "</tr>";
-}
+                $numrow++;
+                }
 
-$numrow++;
-}
+                echo "</tbody></table>";
 
-echo "</tbody></table>";
+                if($kosong > 0){
 
-if($kosong > 0){
+                echo "<div class='alert alert-danger'>
+                    Ada <b>$kosong</b> data yang belum lengkap.
+                </div>";
 
-echo "<div class='alert alert-danger'>
-Ada <b>$kosong</b> data yang belum lengkap.
-</div>";
+                }else{
 
-}else{
+                echo "<button type='submit' name='import' class='btn btn-success' style='border-radius: 30px;'>
+                    Import Data
+                </button>
 
-echo "<button type='submit' name='import' class='btn btn-success'>
-Import Data
-</button>
+                <a href='".site_url("Siswa")."' class='btn btn-danger' style='border-radius: 30px;'>
+                    Cancel
+                </a>";
 
-<a href='".site_url("Siswa")."' class='btn btn-danger'>
-Cancel
-</a>";
+                }
 
-}
-
-echo "</form>";
-}
-?>
-
-</div>
-</div>
+                echo "</form>";
+                }
+            ?>
+        </div>
+    </div>
 </div>
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,63 +1,64 @@
-
-  <div id="app">
+<div id="app">
     <section class="section">
-      <div class="container mt-5">
-        <br><br><br><br><br>
-        <div class="row">
-          <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
-            <?php  
+        <div class="container mt-5">
+            <br><br><br><br><br>
+            <div class="row">
+                <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+                    <?php  
                   if ($this->session->flashdata('msg') == true) {?>
                     <div class="alert alert-danger" role="alert"><?= $this->session->flashdata('msg') ?></div>
-                  <?php }?>
-            <div class="card card-primary" style="border-radius: 5%;">
-              <style>
-                body{
-                  /* background-image:url("image/ihbs-smp.jpg"); */
-                  background-color: #eaeaea;
-                  background-size: cover;
-                }
-              </style>
-              <img src="image/uks1.png" style="width: 100px; margin-left: 34%; margin-top: 5%;" >
-              <!-- <div class="card-header">
+                    <?php }?>
+                    <div class="card card-primary" style="border-radius: 5%;">
+                        <style>
+                        body {
+                            /* background-image:url("image/ihbs-smp.jpg"); */
+                            background-color: #eaeaea;
+                            background-size: cover;
+                        }
+                        </style>
+                        <img src="image/uks1.png" style="width: 100px; margin-left: 34%; margin-top: 5%;">
+                        <!-- <div class="card-header">
                 <h4 style="text-align: center">Unit Kesehatan Sekolah</h4> 
               </div> -->
-            
-              <div class="card-body">
-                <form method="POST" action="<?= base_url('auth/login') ?>" class="needs-validation" novalidate="">
-                  <div class="form-group">
-                    <label for="email">Username</label>
-                    <input id="email" type="text" class="form-control" name="username" tabindex="1" required autofocus>
-                    <div class="invalid-feedback">
-                      Please fill in your username
+
+                        <div class="card-body">
+                            <form method="POST" action="<?= base_url('auth/login') ?>" class="needs-validation"
+                                novalidate="">
+                                <div class="form-group">
+                                    <label for="email">Username</label>
+                                    <input id="email" type="text" class="form-control" name="username" tabindex="1"
+                                        required autofocus>
+                                    <div class="invalid-feedback">
+                                        Please fill in your username
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="d-block">
+                                        <label for="password" class="control-label">Password</label>
+                                    </div>
+                                    <input id="password" type="password" class="form-control" name="password"
+                                        tabindex="2" required>
+                                    <div class="invalid-feedback">
+                                        please fill in your password
+                                    </div>
+                                </div>
+
+                                <div class="form-group; rounded-pill">
+                                    <button type="submit" class="btn btn-primary btn-lg btn-block rounded-pill"
+                                        tabindex="4" style="font-size: medium; border-radius: 30px">
+                                        Login
+                                    </button>
+                                </div>
+
+
+                            </form>
+
+
+                        </div>
                     </div>
-                  </div>
-
-                  <div class="form-group">
-                    <div class="d-block">
-                    	<label for="password" class="control-label">Password</label>
-                    </div>
-                    <input id="password" type="password" class="form-control" name="password" tabindex="2" required>
-                    <div class="invalid-feedback">
-                      please fill in your password
-                    </div>
-                  </div>
-
-                  <div class="form-group; rounded-pill">
-                    <button type="submit" class="btn btn-primary btn-lg btn-block rounded-pill" tabindex="4" style="font-size: medium">
-                      Login
-                    </button>
-                  </div>
-
-                  
-                </form>
-                
-
-              </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </section>
-  </div>
-
-  
+</div>
